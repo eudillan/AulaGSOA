b7c50267cabbfc88512af84302431743ad588abd
eudillan <leonardo_dillan@hotmail.com>
2018-09-20 22:38:27 -0300

    Projeto Java

---
+++ AprendoLinguagememJava/src/TiposDeDados.java
@@ -0,0 +1,19 @@
+public class TiposDeDados{
+    public static void main(String[] args) {
+        //textos
+        char letra = 'a';
+        String texto = "texto";
+        //String começa com letras maiuscula
+        
+        //logico
+        boolean valor = true;
+        
+        //Numérico
+        int inteiro = 10;
+        long inteiroDobrado = 20;
+        float real = 10.0f;
+        double realDobrado = 20.0;
+        
+        
+}
+}
